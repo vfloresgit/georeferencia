3e0b6b0478f9fef3b931f2772300e92562c4e752
vfloresgit <victorfq18@gmail.com>
2018-08-21 16:23:28 -0500

    dlskaj

--- application/controllers/Demo.php
+++ application/controllers/Demo.php
@@ -3,21 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Demo extends CI_Controller {
 
-    function __construct()
-    {   
-    parent::__construct();
-    $this->load->model("Mdireccion");
+    function __construct(){
+
+       parent::__construct();
+       $this->load->model("Mdireccion");
+
     }
 
-	public function index()
-	{
+	public function index(){
+
 		$this->load->view('welcome_message');
+
 	}
 
 	public function texturl(){
          
-
-
+        //TIEMPO DE EJECUCION DEL METODO
 		set_time_limit(0);
 
 
@@ -33,64 +34,52 @@ class Demo extends CI_Controller {
         //  }
 
         //  exit;
+        //print_r($resultado);
 
+        //echo "gfsdhgf";
+
+        $dato = array();
+
+        // echo sizeof($resultado);
 	
-         foreach ($resultado->result() as $lista){
+         foreach ($resultado as $lista){
 
-           $dato[]=array('id'=>$lista->id,'lat'=>$lista->lat,'lon'=>$lista->long);           
-         
+         	if(isset($lista['id'])){
+         		array_push($dato, array('id'=>$lista['id'],'lat'=>$lista['lat'],'lon'=>$lista['lon']));
+         		//echo "OK";
+         	}
+
          }
+         //print_r($dato);
+         // $ini = array('id'=>'ini','lat'=>'-77.068468','lon'=>'-12.037849');   //PARA DIRECCIONES.XLS 
+         $ini = array('id'=>'ini','lat'=>'-78.524212','lon'=>'-7.145127');  
+	     $json=$dato;
 
-
-		  // $ini = array('id'=>'ini','lat'=>'-7.145127','lon'=>'-78.524212');  
-        
-         //$ini = array('id'=>'ini','lat'=>'-77.068468','lon'=>'-12.037849');    
-         $ini = array('id'=>'ini','lat'=>'78.524212','lon'=>'-7.145127');
-	    $json=$dato;
-	    print_r($json);exit;
-
+		// echo "<pre>";
+	 //    print_r($ini);
+  //       echo "</pre>";
+/*
+         echo "<pre>";
+	     print_r($json);
+         echo "</pre>";
+	    
+	   */
 
 		$arr = array("init"=>json_encode($ini),"json"=>json_encode($json));
 
        
-/*      
-		echo "<pre>";
-		print_r($arr);
-		echo "</pre>";*/
+      
+		 // echo "<pre>";
+		 // print_r($arr);
+		 // echo "</pre>";
 
 		$res = $this->curl_base($url,null,$arr,null,null,null,null,null);
-         
-		$re = json_decode($res,TRUE);
-
-			// $id = array_column($re, 'id');
-			// $lat = array_column($re, 'lat');
-			// $lon = array_column($re, 'id');
-			// $id_node = array_column($re, 'id');
- 
-          //  echo "<table border='1'>";
-		        //    echo "<tr>";
-				      //      echo "<td>id</td>";
-				      //      echo "<td>lat</td>";
-				      //      echo "<td>lon</td>";
-				      //      echo "<td>id_node</td>";
-				      //      echo "<td>seq</td>";
-		        //    echo "</tr>";
-          // foreach ($id as $valor) {
-		        //    echo "<tr>";
-				      //      echo "<td>".$valor."</td>";
-				      //      echo "<td></td>";
-				      //      echo "<td></td>";
-				      //      echo "<td></td>";
-				      //      echo "<td></td>";
-		        //    echo "</tr>";
-          // }
-          // echo "</table>";
+		
 
 	
 		echo "<pre>";
-		print_r($re);
+		print_r($res);
 		echo "</pre>";
-
 
 	}
 
@@ -98,13 +87,17 @@ class Demo extends CI_Controller {
 	{
 
 		$a = curl_init($url);
+		// curl_setopt($a, CURLOPT_VERBOSE, true);
 		curl_setopt($a, CURLOPT_POSTFIELDS, $arr);
 		curl_setopt($a, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($a, CURLOPT_BINARYTRANSFER,1);
 		curl_setopt($a, CURLOPT_FRESH_CONNECT, TRUE);
 		curl_setopt($a, CURLOPT_CONNECTTIMEOUT ,2); 
 		$b = curl_exec($a);
+		// $info = curl_getinfo($a);
+		
 		curl_close($a);
+
 		return $b;
 		
 	}
